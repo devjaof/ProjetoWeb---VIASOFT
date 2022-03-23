b9a3d99335c3a4d2c6cbee4173fa5f4d69a34efd
João Francisco Rodrigues <94070775+devjaof@users.noreply.github.com>
2022-03-23 14:24:03 -0300

    Delete backend directory

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-import app from './src/app.js'
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, ()=>{
-    console.log('Escutando em http://localhost:8080')
-})
--- backend/src/config/dbConexao.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://admin:admin@cluster0.hhhkh.mongodb.net/viasoft")
-
-const db = mongoose.connection;
-
-export default db;
