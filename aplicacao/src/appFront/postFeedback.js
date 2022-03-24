9302787267ceec4bc14ad80ad784bae624b0584b
João Francisco Rodrigues <94070775+devjaof@users.noreply.github.com>
2022-03-24 17:01:06 -0300

    Add files via upload

--- aplicacao/src/appFront/postFeedback.js
+++ aplicacao/src/appFront/postFeedback.js
@@ -1,12 +1,19 @@
-const usuario = document.getElementById("inputFeedUsuario").value;
-const data = document.getElementById("inputFeedData").value;
-const melhorar = document.getElementById("inputFeedMelhorar").value;
-const manter = document.getElementById("inputFeedManter").value;
-const sugestoes = document.getElementById("inputFeedSugest").value;
-const remetente = document.getElementById("inputFeedRemet").value;
+var usuario = document.getElementById("inputFeedUsuario");
+var data = document.getElementById("inputFeedData");
+var melhorar = document.getElementById("inputFeedMelhorar");
+var manter = document.getElementById("inputFeedManter");
+var sugestoes = document.getElementById("inputFeedSugest");
+var remetente = document.getElementById("inputFeedRemet");
 
 const btn = document.getElementById("btn");
 
-btn.onclick = function() {
-    console.log(usuario);
+var array = [usuario.value,
+    data.value,
+    melhorar.value,
+    manter.value,
+    sugestoes.value,
+    remetente.value]  
+
+btn.onclick = function (){
+    console.log(array)
 }
